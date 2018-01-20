568fa957f2a98bd0f0986693c764a327d0449f93
Unknown <julia.hermann93@gmail.com>
2018-01-20 23:20:30 +0100

    Fixing secrets

---
+++ src/rest/connection/RecombeeToken.java
@@ -0,0 +1,7 @@
+package rest.connection;
+
+public class RecombeeToken {
+    final static String DB_ID = "sde-final-assignment";
+	final static String SECRET_TOKEN = "LeDMWIM05dYcFydwyKTxJnJbsfwOE7kieL6Hk0oB9VGqzJ3rYCBjhAcV3AMQhpQo";
+
+}
